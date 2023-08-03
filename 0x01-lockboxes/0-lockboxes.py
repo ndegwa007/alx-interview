5514eef3d6fcab9b29ab9f96ba3bde4dc6932f15
ndegz <ndegwa8781@gmail.com>
2023-08-03 11:35:35 +0300

    lockboxes

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+from collections import deque
+
+
+def canUnlockAll(boxes):
+    """checks if all boxes can be opened"""
+    n = len(boxes)
+    visited = [False] * n
+    # first box is open
+    visited[0] = True
+
+    # using BFS create a queue and put the first box
+    queue = deque([0])
+
+    while queue:
+        checked_box = queue.popleft()
+
+        for key in boxes[checked_box]:
+            if not visited[key]:
+                visited[key] = True
+                queue.append(key)
+
+    return all(visited)
---
+++ 0x01-lockboxes/README.md
@@ -0,0 +1,10 @@
+# 0x01.Lockboxes
+
+### Problem Approach
+This problem requires a graph traversal algorithm approach and can be solved using Breadth Firstsearch (BFS) or Depth First Search (DFS) algorithm.
+
+#### Problem
+<details>
+<summary><a href='./0-lockboxes'>0.Lockboxes</a></summary>
+<a href='https://postimg.cc/Wdw90tc8' target='_blank'><img src='https://i.postimg.cc/Wdw90tc8/Screenshot-from-2023-08-03-11-05-35.png' border='0' alt='Screenshot-from-2023-08-03-11-05-35'/></a>
+</details>
