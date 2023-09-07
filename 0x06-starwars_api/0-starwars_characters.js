48d139d7172993982bcc7c02332388057accca57
ndegz <ndegwa8781@gmail.com>
2023-09-07 09:58:12 +0300

    starwars api character names

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,42 @@
+#!/usr/bin/node
+// module has a star wars api
+
+const request = require('request-promise-native');
+const process = require('process');
+
+function getCharacterName (characterUrl) {
+  return new Promise((resolve, reject) => {
+    request(characterUrl)
+      .then((body) => {
+        const characterData = JSON.parse(body);
+        resolve(characterData.name);
+      })
+      .catch((error) => {
+        reject(error);
+      });
+  }
+  );
+}
+
+async function starWars () {
+  const movieId = process.argv[2];
+  const url = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;
+
+  try {
+    const filmData = await request(url);
+    const film = JSON.parse(filmData);
+
+    for (const characterUrl of film.characters) {
+      try {
+        const characterName = await getCharacterName(characterUrl);
+        console.log(`${characterName}`);
+      } catch (characternameError) {
+        console.log(characternameError);
+      }
+    }
+  } catch (filmDataError) {
+    console.log(filmDataError);
+  }
+}
+
+starWars();
---
+++ 0x06-starwars_api/README.md
@@ -0,0 +1,4 @@
+# 0x06.Star Wars API
+
+### Script Context
+Prints all characters in a Star Wars movie
