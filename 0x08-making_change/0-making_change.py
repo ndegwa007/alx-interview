291845ea872e01a7a1ba643d4be78aeaa3e33ae7
ndegz <ndegwa8781@gmail.com>
2023-10-23 10:58:56 +0300

    coin change

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -5,8 +5,9 @@ import sys
 
 def makeChange(coins, total):
     """list  of all possible coins"""
-    if total < 0:
+    if total <= 0:
         return 0
+    
     dp = [sys.maxsize] * (total + 1)
     dp[0] = 0
    
@@ -16,6 +17,4 @@ def makeChange(coins, total):
             if coin <= amount:
                 dp[amount] = min(dp[amount], dp[amount - coin] + 1)
 
-    if dp[total] == sys.maxsize:
-        return -1
     return dp[total]
