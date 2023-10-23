725c230f88acf39a148f616e5ebc21ce8cd6e97d
ndegz <ndegwa8781@gmail.com>
2023-10-23 10:24:09 +0300

    coin change

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""bottom-up approach"""
+import sys
+
+
+def makeChange(coins, total):
+    """list  of all possible coins"""
+
+    dp = [sys.maxsize] * (total + 1)
+    dp[0] = 0
+
+    for amount in range(1, total+1):
+        for coin in coins:
+            if coin <= amount:
+                dp[amount] = min(dp[amount], dp[amount - coin] + 1)
+    if dp[total] > total:
+        return -1
+    return dp[total]
---
+++ 0x08-making_change/README.md
@@ -0,0 +1,6 @@
+# Coin-Change problem
+
+### problem definition
+
+Given a pile of coins of different values, determine the fewest coins needed to meet a given amount total
+
