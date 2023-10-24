30a13a0786feecb3f0439e6d5b448c99f1a9f42b
ndegz <ndegwa8781@gmail.com>
2023-10-24 15:22:50 +0300

    gitignore files

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,48 +1,128 @@
-#!/usr/bin/python3
+#!/usr/bin/python
 """prime game"""
+
+def remove_multiples(nums, prime):
+  """
+  Removes the multiples of a number from a list of numbers.
+
+  Args:
+    nums: A list of numbers.
+    prime: A prime number.
+
+  Returns:
+    A list of numbers with the multiples of the prime number removed.
+  """
+
+  new_nums = []
+  for num in nums:
+    if num % prime != 0:
+      new_nums.append(num)
+
+  return new_nums
+
+
+
+def is_prime(num):
+  """
+  Checks if a number is prime.
+
+  Args:
+    num: A number.
+
+  Returns:
+    True if the number is prime, False otherwise.
+  """
+
+  for i in range(2, int(num**0.5) + 1):
+    if num % i == 0:
+      return False
+
+  return True
+
+
+
+def find_prime(nums):
+  """
+  Finds the first prime number in a list of numbers.
+
+  Args:
+    nums: A list of numbers.
+
+  Returns:
+    The first prime number in the list, or None if there are no prime numbers in the list.
+  """
+
+  for num in nums:
+    if is_prime(num):
+      return num
+
+  return None
 
 
 def isWinner(x, nums):
-    """get winner"""
+  """
+  Determines the winner of the game given the number of rounds and the set of numbers.
 
-    def is_prime(num):
-        """check for primes"""
-        if num <= 1:
-            return False
-        if num <= 3:
-            return True
-        if num % 2 == 0 or num % 3 == 0:
-            return False
-        i = 5
-        while i * i <= num:
-            if num % i == 0 or num % (i + 2) == 0:
-                return False
-            i += 6
-        return True
+  Args:
+    x: The number of rounds.
+    nums: An array of n.
 
-    def calculate_winner(n):
-        """find the winner based on is_prime"""
-        if n <= 1:
-            return "Ben"
-        if n % 2 == 0:
-            return "Ben"
-        if is_prime(n):
-            return 'Maria'
-        return "Ben"
+  Returns:
+    The name of the player that won the most rounds, or None if the winner cannot be determined.
+  """
 
-    maria_wins = 0
-    ben_wins = 0
+  # Create a list to store the number of wins for each player.
+  player_wins = [0, 0]
 
-    for n in nums:
-        winner = calculate_winner(n)
-        if winner == "Maria":
-            maria_wins += 1
-        elif winner == "Ben":
-            ben_wins += 1
+  # Create a set to store the prime numbers that Maria has chosen in each round.
+  maria_chosen_primes = set()
 
-    if maria_wins > ben_wins:
-        return "Maria"
-    elif ben_wins > maria_wins:
-        return "Ben"
+  # Iterate over the number of rounds.
+  for i in range(x):
+
+    # Maria always goes first.
+    maria_turn = True
+
+    # While there are still prime numbers left, the players take turns choosing a prime number and removing it and its multiples from the set.
+    while nums:
+
+      # If it is Maria's turn, she chooses a prime number.
+      if maria_turn:
+        maria_prime = find_prime(nums)
+
+        # If Maria has already chosen the prime number in the same round, she cannot choose it again.
+        if maria_prime in maria_chosen_primes:
+          continue
+
+        # Otherwise, Maria adds the prime number to the set of chosen prime numbers.
+        maria_chosen_primes.add(maria_prime)
+      else:
+        ben_prime = find_prime(nums)
+
+      # Remove the prime number and its multiples from the set.
+      nums = remove_multiples(nums, maria_prime if maria_turn else ben_prime)
+
+      # Switch to the other player's turn.
+      maria_turn = not maria_turn
+
+    # If there are no numbers left in the set, the player whose turn it is wins the round.
+    if not nums:
+      player_wins[0 if maria_turn else 1] += 1
+
+    # Otherwise, the round is a tie.
     else:
-        return None
+      player_wins[0] += 1
+      player_wins[1] += 1
+
+  # If Maria has more wins than Ben, she wins the game.
+  if player_wins[0] > player_wins[1]:
+    return "Maria"
+
+  # If Ben has more wins than Maria, he wins the game.
+  elif player_wins[1] > player_wins[0]:
+    return "Ben"
+
+  # If neither player has more wins than the other, the game is a tie.
+  else:
+    return None
+
