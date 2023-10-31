d388d13bce8afbcb168b8fae86e6be5292caa741
ndegz <ndegwa8781@gmail.com>
2023-10-31 09:27:49 +0300

    fixes

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -6,31 +6,43 @@ import sys
 
 def main_log():
     """compute stats in each line"""
-    file_size_total = 0
     line_count = 0
     status_code_dict = {}
     total = 0
-
-   
+    file_size_total = 0
 
     try:
+
         for line in sys.stdin:
             line = line.split(' ')
-            line_count +=1
-
             file_size = line[len(line)-1]
             status_code = line[len(line)-2]
+
             if status_code in status_code_dict:
                 status_code_dict[status_code] += 1
             else:
                 status_code_dict[status_code] = 1
+
             file_size_total += int(file_size)
-            if line_count % 10 == 0:
+
+            if (line_count + 1) % 10 == 0 and line_count > 0:
                 total += file_size_total
                 print(f"File size: {total}")
                 for k, v in sorted(status_code_dict.items()):
                     print(f"{k}: {v}")
                 file_size_total = 0
+
+            line_count += 1
+
+        if line_count == 1:
+            # Print specific information only when line_count is exactly 0
+            print(f"File size: {file_size_total}")
+            for k, v in sorted(status_code_dict.items()):
+                print(f"{k}: {v}")
+
+        if line_count == 0:
+            print("File size: 0")
+
     except KeyboardInterrupt:
         print(f"File size: {file_size_total}")
         for k, v in sorted(status_code_dict.items()):
